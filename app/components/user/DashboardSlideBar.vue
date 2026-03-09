820e9c661d651db3443045985dbfc3d8a1bf0983
gitzno <gitzno>
2026-03-09 14:25:10 +0700

    SetUp: Nuxt

--- app/components/user/DashboardSlideBar.vue
+++ app/components/user/DashboardSlideBar.vue
@@ -103,7 +103,7 @@ export default {
                                     path: "/user/posts",
                                     query: { category: "news" },
                                 },
-                            },
+                             },
                         ],
                     },
                 ],
